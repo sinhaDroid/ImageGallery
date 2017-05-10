173ea2d79fc30abdf21e1b12e1c67101113c6bab
sinhadroid <107sinha@gmail.com>
2017-05-10 09:18:28 +0530

    added fragment into activity

--- app/src/main/java/com/example/deepanshu/imagegallery/MainActivity.java
+++ app/src/main/java/com/example/deepanshu/imagegallery/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.deepanshu.imagegallery;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.deepanshu.imagegallery.gallery.view.GalleryFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().
+                beginTransaction()
+                .replace(R.id.container, GalleryFragment.newInstance())
+                .commitAllowingStateLoss();
     }
 }
